b6a56a3d5e3a786dfc1f4ba113d94f1b60b9f910
Shreyas <shreyasls@yahoo.com>
2021-12-20 16:50:07 -0500

    Export search as a service

--- app/services.mjs
+++ app/services.mjs
@@ -1,2 +1,3 @@
 export * as collections from './modules/collections.mjs';
 export * as indexer     from './modules/indexer-of-files.mjs';
+export * as search      from './modules/search-and-fetch.mjs';
