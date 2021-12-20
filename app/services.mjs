32d508568039e9e3883985ed00394bf13a82982a
Shreyas <shreyasls@yahoo.com>
2021-12-19 22:46:41 -0500

    Cleaned up services to remove old calls

--- app/services.mjs
+++ app/services.mjs
@@ -1,12 +1,2 @@
-import * as db from './database/sqlite-database.mjs';
-
-export function isFirstTimeRun(){
-  return db.checkDbExists() ? false : true;
-}
-
-export function firstTimeSetup(){
-  db.dbSetup()
-}
-
 export * as collections from './modules/collections.mjs';
 export * as indexer from './modules/indexer-of-files.mjs';
