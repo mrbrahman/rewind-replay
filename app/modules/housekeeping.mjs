a647384b927bb04f4d7d81f1966c0be21a1c6704
Shreyas <shreyasls@yahoo.com>
2022-01-03 08:38:51 -0500

    Added TODO item

--- app/modules/housekeeping.mjs
+++ app/modules/housekeeping.mjs
@@ -1,7 +1,9 @@
 import {exiftool} from 'exiftool-vendored';
-import {fileWatcherManagement} from './watcher.mjs'
+import {fileWatcherManagement} from './watcher.mjs';
 
 export function shutdownCleanup(){
+  // TODO: commit any pending changes
+  // dbMetadata.runNow();
   exiftool.end();
   fileWatcherManagement.stopAllWatchers();
 }
