1b17114c88479437805124f282cf286f83ea1be6
Shreyas <shreyasls@yahoo.com>
2022-09-30 08:33:27 -0400

    Refactored search. Removed search-query-parser dependency

--- app/database/search-db.mjs
+++ app/database/search-db.mjs
@@ -1,10 +1,21 @@
-import * as parser from 'search-query-parser';
-
 import { db } from './sqlite-database.mjs';
 
-function converToSQLQueryStr(searchStr){
+export const restrictSearchCols = ['album', 'keywords', 'faces', 'objects', 'mediatype', 'rating', 'make', 'model'];
+
+// aliases: the right side (realCol) can also be known by the left side (alias)
+export const aliases = {
+  tags: 'keywords',
+  people: 'faces',
+  name: 'faces',
+  face: 'faces',
+  camera: 'model',
+  type: 'mediatype',
+  l: 'logical'
+}
+
+function converToFilterStr(searchStr){
   /*
-    First cut features:
+    Features:
     1. When multiple conditions are prsent, by default they are "AND"ed.
         e.g. album:trip camera:samsung type:video
         will translate as
@@ -14,117 +25,67 @@ function converToSQLQueryStr(searchStr){
         e.g. album:trip camera:samsung type:video l:or
         will translate as
         {album}: "trip"* OR {camera}: "samsung"* OR {type}: "video"*
-    4. Input for a particular condition can contain multiple values, in which case they
-       are "OR"ed.
-        e.g. album:trip,vacation
-        will translate as
-        {album}: ("trip"* OR "vacation"*)
-       It is not possible to change this behavior, and make this AND (currently)
-    5. Any un-prefixed condition will be applied to all search-enabled columns
+    4. Any un-prefixed condition will be applied to all search-enabled columns
        in the restrictSearchCols array
-    6. For advanced needs (including querying non restricted columns), use the "raw"
+    5. For advanced needs (including querying non restricted columns - for e.g. file_date), use the "raw"
        input using SQLite FTS syntax. Thich will be used as-is in the filter.
-        e.g. raw:"{album}: (states* AND trip*)"
-       Note: there are quoting issues in search-query-parser library :-/
+        e.g. 
+          raw:"metadata match '{album}: (states* AND trip*)'"
+          raw:"strftime('%W',file_date)=strftime('%W',date()) and strftime('%Y',file_date) != strftime('%Y',date())" --> all 'past' photos of current week
+    6. "raw" can be clubbled with other filters, if needed
+  
+    TODO: implement faces (array columns) OR, AND and ONLY conditions
   */
 
-  // TODO: implement facesLogical to contain OR, AND and ONLY conditions
-  // implement date search including ranges
-
-  const options = {
-    keywords: ['album', 'keywords', 'tags', 'people', 'name', 'faces', 'face', 'type', 'mediatype', 'objects', 'rating', 'camera', 'make', 'model', 'date', 'on', 'l', 'logical', 'raw'],
-    //ranges: ['between', 'dates'],  TODO:?
-    alwaysArray: true,
-    tokenize: true,
-    offsets: false
-  };
-
-  // aliases: the right side (realCol) can also be known by the left side (alias)
-  const aliases = {
-    tags: 'keywords',
-    people: 'faces',
-    name: 'faces',
-    face: 'faces',
-    camera: 'model',
-    type: 'mediatype',
-    on: 'file_date',
-    l: 'logical'
-  }
-
-  var parsedInput = parser.parse(searchStr, options);
-
-  if (parsedInput.raw){
-    return parsedInput.raw[0];
-    // nothing else to do, ignore everything else given
-  }
-
-  // if query has used aliases, move them to appropriate real column
-  for(const [alias,realCol] of Object.entries(aliases)){
-    if(parsedInput[alias]){
-      // the alias is found in search query
-      if(parsedInput[realCol]){
-        // there is an entry already for that realCol, hence join the 2 arrays
-        parsedInput[realCol] = parsedInput[realCol].concat(parsedInput[alias])
-      } else {
-        // there is no entry for that realCol, make a new one
-        parsedInput[realCol] = parsedInput[alias]
-      }
-      // now remove the alias
-      delete parsedInput[alias];
-    }
-  }
-  // also delete exclude until it is implemented
-  delete parsedInput.exclude;
-
-  // set "logical" and "facesLogical" values. These will be used during search
-  let logical = parsedInput['logical'] ?
-    parsedInput['logical'][0].toUpperCase() : // remember we set "alwaysArray: true" in search options
-    'AND'  // by default, we use AND condition
-  ;
-  delete parsedInput.logical;
-
-  // facesLogical valid values: OR, AND, ONLY
-  // attempting to eventually implement:
-  // OR: face1 or face2 must be present (others may also be present)
-  // AND: face1 and face2 must be present (others may also be present)
-  // ONLY: face1 and face2 must be present, and no others
-  //    (this helps to identify family pics with only the required members)
-
-  // let facesLogical=''
-  // if(parsedInput['faces']){
-  //   if(parsedInput['facesLogical']){
-  //     facesLogical = parsedInput['facesLogical'][0].toUpperCase();
-  //     delete parsedInput.facesLogical;
-  //   } else {
-  //     facesLogical = 'OR'
-  //   }
-  // }
-
-  console.log(parsedInput);
-  const restrictSearchCols = ['album', 'keywords', 'faces', 'objects', 'mediatype', 'rating', 'make', 'model', 'file_date'];
-
-  // TODO: make this better?
-  let conditions = [];
-  for (let [col,val] of Object.entries(parsedInput)){
-    let c = '',
-      valQuotedAndStarred = val.map(x=>`"${x}"*`);
-
-    if(restrictSearchCols.indexOf(col) >=0 ){
-      c = `{${col}} : (${valQuotedAndStarred.join(' OR ')})`
-    } else if (col == 'text'){
-      // no prefix provided
-      c = `{${restrictSearchCols.join(' ')}} : ( ${valQuotedAndStarred.join(' OR ')} )`
+  // split appropriately by space (considering space inside of a string), and split into array
+  let filterItems = searchStr.replaceAll(/\s+(?=(?:(?:[^"]*"){2})*[^"]*"[^"]*$)/g, "__s_p_a_c_e__").split(/\s+/).map(x=>x.replaceAll(/__s_p_a_c_e__/g, ' '));
+  
+  let filterKeyVal = filterItems.map(x=>{
+    let [first, ...rest] = x.split(':');
+    if(rest.length>0){
+      return [first, rest.join(':')];
     }
     else {
-      // silently ignore other nonsense
+      return [first]
     }
+  });
 
-    // add to conditions, if one is set
-    if(c) conditions.push(c);
+  let strip = (s) => s.replace(/(^"|"$)/g, '');
+  
+
+  let logical='AND', ftsFilters=[], otherFilters=[];
+  for (let f of filterKeyVal) {
+    let col = (aliases[f[0]] || f[0]).toLowerCase();
+    let filterStr = (f[1] && strip(f[1])) || null;
+    
+    if (f.length == 1){
+      // search across all allowed columns
+      ftsFilters.push(`{${restrictSearchCols.join(' ')}} : ( "${strip(f[0])}"* )`);
+    }
+    else if(col == 'logical'){
+      logical = filterStr.toUpperCase();
+    }
+    else if (col == "raw"){
+      otherFilters.push(filterStr);
+    }
+    else if(restrictSearchCols.includes(col)){
+      // TODO: handle , (and) | (or) & (only)
+      
+      // "only" is appliable only for array data
+      ftsFilters.push(`{${col}} : ( "${filterStr}"* )`)
+    }
+    else {
+      // ignore everything else
+    }
   }
 
-  let final = conditions.join(logical);
-  return `metadata match '${final}'`
+  let allFtsFilters = ftsFilters.length > 0 ? `metadata match '${ftsFilters.join(` ${logical} `)}'` : '';
+  let allOtherFilters = otherFilters.join(` ${logical} `)
+
+  let allFilters = [allFtsFilters, allOtherFilters].filter(x=>x);
+
+  let final = `( ${allFilters.join(` ${logical} `)} )`;
+  return final;
 
 }
 
@@ -135,7 +96,7 @@ export function runSearch(collection_id, searchStr){
     filters.push(`collection_id = ${collection_id}`);
 
   if(searchStr){
-    let parsedCondition = converToSQLQueryStr(searchStr);
+    let parsedCondition = converToFilterStr(searchStr);
     console.log(parsedCondition);
 
     filters.push(parsedCondition)
